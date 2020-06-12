35b658f95f562bccd87729e5ab5e78d1bab077a9
louise.kirkham <louise.kirkham@royalmail.com>
2020-06-12 09:48:37 +0000

    add code in files types: .py,.pb,.json,.pem,.proto,.bazel,.yaml,.md,.bzl,.sh,,.csh,.fish,.7,.cfg remove code in files types:

---
+++ content.md
@@ -0,0 +1,1 @@
+hsbdmboony vmwoqwkydu ioxhlueqvs
---
+++ content.py
@@ -0,0 +1,17 @@
+# Contributions-Importer-For-Github/run_script.py
+import git
+from git_contributions_importer import *
+
+# Your private repo or Bitbucket repo
+repo = git.Repo("/Users/louisekirkham/Documents/projects/Vroom/vehicle-route-optimiser")
+
+# Your mock repo
+mock_repo = git.Repo("/Users/louisekirkham/Documents/my_projects/mock_repo_rm_vrm")
+importer = Importer([repo], mock_repo)
+
+# I use both my personal email and work email here,
+# Since the private repo uses work email, and Github profiles uses
+# my work email
+importer.set_author(['louisek@gmail.com', 'louise.kirkham@royalmail.com'])
+importer.import_repository()
+print("ypeiu")
